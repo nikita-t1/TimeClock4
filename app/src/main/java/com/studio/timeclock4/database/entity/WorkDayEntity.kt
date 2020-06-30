158b44848309e136e898a47f8c5b327ba498c919
nikita-t1 <nikita.t2@gmx.de>
2020-06-30 19:24:11 +0200

    Add WorkDayEntity data class

---
+++ app/src/main/java/com/studio/timeclock4/database/entity/WorkDayEntity.kt
@@ -0,0 +1,31 @@
+package com.studio.timeclock4.database.entity
+
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import com.studio.timeclock4.database.entity.WorkDayEntity.Companion.TABLE_NAME
+import org.threeten.bp.Duration
+import org.threeten.bp.LocalDate
+
+@Entity(tableName = TABLE_NAME)
+data class WorkDayEntity(
+
+    @PrimaryKey
+    val date: LocalDate,
+
+    val baseTime: Duration,
+    var pauseTime: Duration,
+    var workTimeNet: Duration,
+    var absenceID: Long? = null
+
+) {
+    @Ignore
+    val workTimeGross: Duration = workTimeNet.plus(pauseTime)
+
+    @Ignore
+    val overtime: Duration = workTimeGross.minus(baseTime)
+
+    companion object {
+        const val TABLE_NAME = "workday_table"
+    }
+}
