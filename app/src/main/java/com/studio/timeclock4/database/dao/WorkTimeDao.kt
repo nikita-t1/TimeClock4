16e0adcd169b22badebf5af5b123295f5727e90d
nikita-t1 <nikita.t2@gmx.de>
2020-07-01 11:21:19 +0200

    Add WorkTimeDao

---
+++ app/src/main/java/com/studio/timeclock4/database/dao/WorkTimeDao.kt
@@ -0,0 +1,34 @@
+package com.studio.timeclock4.database.dao
+
+import androidx.room.*
+import com.studio.timeclock4.database.entity.WorkTimeEntity
+import kotlinx.coroutines.flow.Flow
+import org.threeten.bp.LocalDate
+
+@Dao
+interface WorkTimeDao {
+
+    @Query("SELECT * FROM ${WorkTimeEntity.TABLE_NAME}")
+    fun getWorkTimesObservable(): Flow<List<WorkTimeEntity>>
+
+    @Query("SELECT * FROM ${WorkTimeEntity.TABLE_NAME}")
+    fun getWorkTimes(): List<WorkTimeEntity?>
+
+    @Query("SELECT * FROM ${WorkTimeEntity.TABLE_NAME} WHERE workTimeID = :id")
+    fun getWorkTimeByID(id: Long): WorkTimeEntity?
+
+    @Query("SELECT * FROM ${WorkTimeEntity.TABLE_NAME} WHERE workTimeOwnerDate = :ownerDate")
+    fun getWorkTimeByOwnerDate(ownerDate: LocalDate): List<WorkTimeEntity?>
+
+    @Query("SELECT * FROM ${WorkTimeEntity.TABLE_NAME} WHERE workTimeOwnerDate = :ownerDate")
+    fun getWorkTimeByOwnerDateObservable(ownerDate: LocalDate): Flow<List<WorkTimeEntity?>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertWorkTime(workTime: WorkTimeEntity): Long
+
+    @Update
+    fun updateWorkTime(workTime: WorkTimeEntity)
+
+    @Delete
+    fun deleteWorkTime(workTime: WorkTimeEntity)
+}
