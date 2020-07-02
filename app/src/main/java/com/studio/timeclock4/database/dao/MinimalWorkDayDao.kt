ebf36d9c45b0227ca754aae640ca43441cd31e56
nikita-t1 <nikita.t2@gmx.de>
2020-07-02 10:43:29 +0200

    Add MinimalWorkDayDao

---
+++ app/src/main/java/com/studio/timeclock4/database/dao/MinimalWorkDayDao.kt
@@ -0,0 +1,41 @@
+package com.studio.timeclock4.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+import com.studio.timeclock4.database.entity.WorkDayEntity
+import com.studio.timeclock4.database.model.MinimalWorkDay
+import kotlinx.coroutines.flow.Flow
+import org.threeten.bp.LocalDate
+
+@Dao
+interface MinimalWorkDayDao {
+
+    @Transaction
+    @Query("SELECT * FROM ${WorkDayEntity.TABLE_NAME}")
+    fun getMinimalWorkDaysObservable(): Flow<List<MinimalWorkDay>>
+
+    @Transaction
+    @Query("SELECT * FROM ${WorkDayEntity.TABLE_NAME}")
+    fun getMinimalWorkDays(): List<MinimalWorkDay?>
+
+    @Transaction
+    @Query("SELECT * FROM ${WorkDayEntity.TABLE_NAME} WHERE datetime(date) = datetime(:date)")
+    fun getWorkDayByDate(date: LocalDate): MinimalWorkDay?
+
+    @Transaction
+    @Query(
+        """SELECT * FROM ${WorkDayEntity.TABLE_NAME} WHERE 
+        datetime(date) BETWEEN datetime(:startDate) AND datetime(:endDate)"""
+    )
+    fun getMinimalWorkDaysBetween(startDate: LocalDate, endDate: LocalDate): List<MinimalWorkDay?>
+
+    @Transaction
+    @Query(
+        """SELECT * FROM ${WorkDayEntity.TABLE_NAME} WHERE 
+        datetime(date) BETWEEN datetime(:startDate) AND datetime(:endDate)"""
+    )
+    fun getMinimalWorkDaysObservableBetween(
+        startDate: LocalDate, endDate: LocalDate
+    ): Flow<List<MinimalWorkDay?>>
+}
