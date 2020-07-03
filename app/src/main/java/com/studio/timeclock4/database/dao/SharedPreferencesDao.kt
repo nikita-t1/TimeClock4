4c7eeb1c0f4fd0293d7469d653a7180f75b93e94
nikita-t1 <nikita.t2@gmx.de>
2020-07-03 18:43:46 +0200

    Add SharedPreferencesDao

---
+++ app/src/main/java/com/studio/timeclock4/database/dao/SharedPreferencesDao.kt
@@ -0,0 +1,70 @@
+package com.studio.timeclock4.database.dao
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object SharedPreferencesDao {
+
+    private const val PREFS_NAME = "params"
+    private lateinit var prefs: SharedPreferences
+
+    fun init(context: Context) {
+        prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    }
+
+    fun read(key: String, value: String): String {
+        return prefs.getString(key, value) ?: ""
+    }
+
+    fun read(key: String, value: Boolean): Boolean {
+        return prefs.getBoolean(key, value)
+    }
+
+    fun read(key: String, value: Int): Int {
+        return prefs.getInt(key, value)
+    }
+
+    fun read(key: String, value: Long): Long {
+        return prefs.getLong(key, value)
+    }
+
+    fun read(key: String, value: Float): Float {
+        return prefs.getFloat(key, value)
+    }
+
+    fun write(key: String, value: String) {
+        val prefsEditor: SharedPreferences.Editor = prefs.edit()
+        prefsEditor.putString(key, value)
+        prefsEditor.apply()
+    }
+
+    fun write(key: String, value: Boolean) {
+        val prefsEditor: SharedPreferences.Editor = prefs.edit()
+        prefsEditor.putBoolean(key, value)
+        prefsEditor.apply()
+    }
+
+    fun write(key: String, value: Int) {
+        val prefsEditor: SharedPreferences.Editor = prefs.edit()
+        prefsEditor.putInt(key, value)
+        prefsEditor.apply()
+    }
+
+    fun write(key: String, value: Long) {
+        val prefsEditor: SharedPreferences.Editor = prefs.edit()
+        prefsEditor.putLong(key, value)
+        prefsEditor.apply()
+    }
+
+    fun write(key: String, value: Float) {
+        val prefsEditor: SharedPreferences.Editor = prefs.edit()
+        prefsEditor.putFloat(key, value)
+        prefsEditor.apply()
+    }
+
+    fun remove(key: String) {
+        val prefsEditor: SharedPreferences.Editor = prefs.edit()
+        prefsEditor.remove(key)
+        prefsEditor.apply()
+    }
+}
