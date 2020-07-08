f1e9b462f7faf8cbfe194121e81e27ab083b587c
nikita-t1 <nikita.t2@gmx.de>
2020-07-08 21:49:20 +0200

    Extensions File -> Add Long to TimeFormat

--- app/src/main/java/com/studio/timeclock4/utils/Extensions.kt
+++ app/src/main/java/com/studio/timeclock4/utils/Extensions.kt
@@ -3,9 +3,24 @@ package com.studio.timeclock4.utils
 import org.threeten.bp.Duration
 import org.threeten.bp.LocalTime
 import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.temporal.ChronoUnit
+import kotlin.math.floor
 
 fun Duration.format(pattern: DateTimeFormatter): String{
     val seconds = this.seconds
+    val localTime = LocalTime.ofSecondOfDay(seconds)
+    return localTime.format(pattern)
+}
+
+fun Long.format(pattern: DateTimeFormatter, unit: ChronoUnit): String{
+    val seconds = when (unit){
+        ChronoUnit.NANOS -> this * NUMBER_NANOSECONDS_IN_SECONDS
+        ChronoUnit.MICROS -> this * NUMBER_MICROSECONDS_IN_SECONDS
+        ChronoUnit.MILLIS -> this * NUMBER_MILLISECONDS_IN_SECOND
+        ChronoUnit.SECONDS -> this
+        ChronoUnit.MINUTES -> floor(this * NUMBER_MINUTES_IN_SECONDS).toLong()
+        else -> 0
+    }
     val localTime = LocalTime.ofSecondOfDay(seconds)
     return localTime.format(pattern)
 }
