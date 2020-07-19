f00e5976f8b60914a92e88498c250f2aa4594d1f
nikita-t1 <nikita.t2@gmx.de>
2020-07-19 17:51:40 +0200

    Extended Extensions.kt
    Added Extensions for Toast Creation and for View Visibility

--- app/src/main/java/com/studio/timeclock4/utils/Extensions.kt
+++ app/src/main/java/com/studio/timeclock4/utils/Extensions.kt
@@ -1,5 +1,9 @@
 package com.studio.timeclock4.utils
 
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import es.dmoral.toasty.Toasty
 import org.threeten.bp.Duration
 import org.threeten.bp.LocalTime
 import org.threeten.bp.format.DateTimeFormatter
@@ -14,13 +18,29 @@ fun Duration.format(pattern: DateTimeFormatter): String{
 
 fun Long.format(pattern: DateTimeFormatter, unit: ChronoUnit): String{
     val seconds = when (unit){
-        ChronoUnit.NANOS -> this * NUMBER_NANOSECONDS_IN_SECONDS
-        ChronoUnit.MICROS -> this * NUMBER_MICROSECONDS_IN_SECONDS
-        ChronoUnit.MILLIS -> this * NUMBER_MILLISECONDS_IN_SECOND
+        ChronoUnit.NANOS -> this / NUMBER_NANOSECONDS_IN_SECONDS
+        ChronoUnit.MICROS -> this / NUMBER_MICROSECONDS_IN_SECONDS
+        ChronoUnit.MILLIS -> this / NUMBER_MILLISECONDS_IN_SECOND
         ChronoUnit.SECONDS -> this
         ChronoUnit.MINUTES -> floor(this * NUMBER_MINUTES_IN_SECONDS).toLong()
         else -> 0
     }
     val localTime = LocalTime.ofSecondOfDay(seconds)
     return localTime.format(pattern)
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun Fragment.toast(message: String, duration: Int = Toasty.LENGTH_SHORT) {
+    Toasty.info(requireContext(), message, duration).show()
+}
+
+fun AppCompatActivity.toast(message: String, duration: Int = Toasty.LENGTH_SHORT) {
+    Toasty.info(this, message, duration).show()
 }
